1cca20cc9b964ada543a08d61a4e6419a384fe51
Carl-Emil kjellstrand <carl-emil.kjellstrand@thinnikatech.se>
2024-09-06 14:27:22 +0200

    Adding missing mockserver dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -120,4 +120,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
+
 }
