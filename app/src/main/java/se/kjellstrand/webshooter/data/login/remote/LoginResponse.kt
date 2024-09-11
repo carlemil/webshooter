09cfb161de326a7f4a9f9896428c03cc98024e38
Carl-Emil kjellstrand <carl-emil.kjellstrand@thinnikatech.se>
2024-09-11 13:29:20 +0200

    Moved response out from parsing.

--- app/src/main/java/se/kjellstrand/webshooter/data/login/remote/LoginResponse.kt
+++ app/src/main/java/se/kjellstrand/webshooter/data/login/remote/LoginResponse.kt
@@ -1,8 +1,25 @@
 package se.kjellstrand.webshooter.data.login.remote
 
-data class LoginResponse(
-    val token_type: String,
-    val expires_in: Int,
-    val access_token: String,
-    val refresh_token: String
-)
+import com.beust.klaxon.*
+
+private val klaxon = Klaxon()
+
+data class LoginResponse (
+    @Json(name = "token_type")
+    val tokenType: String,
+
+    @Json(name = "expires_in")
+    val expiresIn: Long,
+
+    @Json(name = "access_token")
+    val accessToken: String,
+
+    @Json(name = "refresh_token")
+    val refreshToken: String
+) {
+    public fun toJson() = klaxon.toJsonString(this)
+
+    companion object {
+        public fun fromJson(json: String) = klaxon.parse<LoginResponse>(json)
+    }
+}
