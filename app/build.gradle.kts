55c75f68858607063b3b278f10a2f70589aa9c11
Carl-Emil kjellstrand <carl-emil.kjellstrand@thinnikatech.se>
2024-09-26 09:40:54 +0200

    Moving even more deps to libs.versions.toml and updating versions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform("androidx.compose:compose-bom:2024.09.02"))
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
@@ -79,16 +79,11 @@ dependencies {
     // Navigation
     implementation(libs.androidx.navigation.compose)
 
-    // Room
-    // implementation("androidx.room:room-ktx:2.6.0")
-    // kapt("androidx.room:room-compiler:2.6.0")
-    // implementation("androidx.room:room-paging:2.6.0")
-
     // Dagger - Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-compiler:2.44")
-    kapt("androidx.hilt:hilt-compiler:1.1.0")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    kapt(libs.androidx.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
