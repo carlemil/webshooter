42b579982668e855acbe04f71f90f81c8704d067
Carl-Emil Kjellstrand <carl-emil.kjellstrand@thinnikatech.se>
2024-10-24 16:05:59 +0200

    Remove login page from backstack once logged in.

--- app/src/main/java/se/kjellstrand/webshooter/ui/navigation/NavigationRoutes.kt
+++ app/src/main/java/se/kjellstrand/webshooter/ui/navigation/NavigationRoutes.kt
@@ -1,13 +1,13 @@
 package se.kjellstrand.webshooter.ui.navigation
 
 sealed class Screen(val route: String) {
-    object LoginScreen : Screen("login_screen")
-    object LandingScreen : Screen("landing_screen")
-    object CompetitionsList : Screen("competitions_list")
-    object CompetitionDetail : Screen("competition_detail/{competitionId}") {
+    data object LoginScreen : Screen("login_screen")
+    data object LandingScreen : Screen("landing_screen")
+    data object CompetitionsList : Screen("competitions_list")
+    data object CompetitionDetail : Screen("competition_detail/{competitionId}") {
         fun createRoute(competitionId: Long) = "competition_detail/$competitionId"
     }
-    object CompetitionResults : Screen("competition_results/{competitionId}") {
+    data object CompetitionResults : Screen("competition_results/{competitionId}") {
         fun createRoute(competitionId: Long) = "competition_results/$competitionId"
     }
 }
