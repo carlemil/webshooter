bc4b7f4e5c283a1b3e772739a08a852752f8d272
Carl-Emil Kjellstrand <carl-emil.kjellstrand@thinnikatech.se>
2024-10-25 14:18:01 +0200

    Moved interceptors out to their own files.

---
+++ app/src/main/java/se/kjellstrand/webshooter/data/AuthCookieJar.kt
@@ -0,0 +1,31 @@
+package se.kjellstrand.webshooter.data
+
+import okhttp3.Cookie
+import okhttp3.CookieJar
+import okhttp3.HttpUrl
+
+class AuthCookieJar : CookieJar {
+    private val cookieStore: MutableMap<String, Cookie> = mutableMapOf()
+
+    override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+        println("saveFromResponse: $cookies")
+        for (cookie in cookies) {
+            val key = "${cookie.domain}${cookie.path}${cookie.name}"
+            cookieStore[key] = cookie
+        }
+    }
+
+    override fun loadForRequest(url: HttpUrl): List<Cookie> {
+        println("loadForRequest: $cookieStore")
+        return cookieStore.values.filter { cookie ->
+            url.host.endsWith(cookie.domain) && url.encodedPath.startsWith(cookie.path)
+        }
+    }
+
+    fun getAllCookies(): String {
+        val cookies = cookieStore.values.map { cookie ->
+            "${cookie.name}=${cookie.value}"
+        }.joinToString(separator = "; ")
+        return cookies
+    }
+}
---
+++ app/src/main/java/se/kjellstrand/webshooter/data/CookieHeadersInterceptor.kt
@@ -0,0 +1,22 @@
+package se.kjellstrand.webshooter.data
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class CookieHeadersInterceptor @Inject constructor(
+    private val cookieJar: AuthCookieJar
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+
+        val originalRequest = chain.request()
+
+        val allCookies = cookieJar.getAllCookies()
+
+        val requestBuilder = originalRequest.newBuilder()
+        requestBuilder.header("Cookie", allCookies)
+
+        val newRequest = requestBuilder.build()
+        return chain.proceed(newRequest)
+    }
+}
