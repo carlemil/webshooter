902399dea8e8f1c905b2cfedb61519f6f8e046d5
Carl-Emil Kjellstrand <carl-emil.kjellstrand@thinnikatech.se>
2026-02-07 12:07:00 +0100

    Fixed parsing for cvä class.

--- app/src/main/java/se/kjellstrand/webshooter/data/common/WeaponClass.kt
+++ app/src/main/java/se/kjellstrand/webshooter/data/common/WeaponClass.kt
@@ -19,24 +19,24 @@ data class WeaponClass(
 )
 
 enum class ClassnameGeneral(val value: String) {
-    A("A"),
-    B("B"),
-    C("C"),
-    CD("CD"),
-    CJun("CJun"),
-    Cvy("CVY"),
-    Cvä("CVÄ"),
-    R("R"),
-    M1("M1"),
-    M2("M2"),
-    M3("M3"),
-    M4("M4"),
-    M5("M5"),
-    M6("M6"),
-    M7("M7"),
-    M8("M8"),
-    M9("M9"),
-    OptR("OptR");
+    @SerializedName("A") A("A"),
+    @SerializedName("B") B("B"),
+    @SerializedName("C") C("C"),
+    @SerializedName("CD") CD("CD"),
+    @SerializedName("CJun") CJun("CJun"),
+    @SerializedName("CVY") Cvy("CVY"),
+    @SerializedName("CVÄ") Cvä("CVÄ"),
+    @SerializedName("R") R("R"),
+    @SerializedName("M1") M1("M1"),
+    @SerializedName("M2") M2("M2"),
+    @SerializedName("M3") M3("M3"),
+    @SerializedName("M4") M4("M4"),
+    @SerializedName("M5") M5("M5"),
+    @SerializedName("M6") M6("M6"),
+    @SerializedName("M7") M7("M7"),
+    @SerializedName("M8") M8("M8"),
+    @SerializedName("M9") M9("M9"),
+    @SerializedName("OptR") OptR("OptR");
 
     companion object {
         public fun fromValue(value: String): ClassnameGeneral = when (value) {
@@ -46,7 +46,7 @@ enum class ClassnameGeneral(val value: String) {
             "CD" -> CD
             "CJun" -> CJun
             "CVY" -> Cvy
-            "CVÄ" -> Cvä
+            "CVÄ", "CV\u00c4" -> Cvä
             "R" -> R
             "M1" -> M1
             "M2" -> M2
@@ -58,7 +58,7 @@ enum class ClassnameGeneral(val value: String) {
             "M8" -> M8
             "M9" -> M9
             "OptR" -> OptR
-            else -> throw IllegalArgumentException()
+            else -> throw IllegalArgumentException("Unknown ClassnameGeneral value: $value")
         }
     }
 }
