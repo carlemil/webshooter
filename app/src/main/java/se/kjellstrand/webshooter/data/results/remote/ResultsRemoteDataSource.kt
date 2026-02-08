6e11693f497fcbeba76710d6fcab523476fc5676
Carl-Emil Kjellstrand <carl-emil.kjellstrand@thinnikatech.se>
2026-02-08 12:56:29 +0100

    Fixed broken results call, was post should be get. and not /app but /api/v4.1.9

--- app/src/main/java/se/kjellstrand/webshooter/data/results/remote/ResultsRemoteDataSource.kt
+++ app/src/main/java/se/kjellstrand/webshooter/data/results/remote/ResultsRemoteDataSource.kt
@@ -1,12 +1,13 @@
 package se.kjellstrand.webshooter.data.results.remote
 
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ResultsRemoteDataSource {
 
-    @POST("api/v4.1.9/competitions/{id}/results")
+    @GET("/api/v4.1.9/competitions/{id}/results")
     suspend fun getResults(
         @Header("Authorization") token: String,
         @Path("id") id: Int,
