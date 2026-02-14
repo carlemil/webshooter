4a0b3ef5a371a657e39f443bb5665162f31c0c7a
Carl-Emil Kjellstrand <carl-emil.kjellstrand@thinnikatech.se>
2026-02-14 13:21:20 +0100

    Shooter results screen is now working, but needs more work.

--- app/src/main/java/se/kjellstrand/webshooter/data/results/ResultsRepository.kt
+++ app/src/main/java/se/kjellstrand/webshooter/data/results/ResultsRepository.kt
@@ -15,7 +15,7 @@ import javax.inject.Singleton
 open class ResultsRepository @Inject constructor(
     private val resultsRemoteDataSource: ResultsRemoteDataSource
 ) {
-    private var resultsCache = null as? ResultsResponse
+    private val resultsCache = mutableMapOf<Int, ResultsResponse>()
 
     fun get(competitionId: Int): Flow<Resource<ResultsResponse, UserError>> {
         return flow {
@@ -36,25 +36,43 @@ open class ResultsRepository @Inject constructor(
                 emit(Resource.Error(UserError.UnknownError))
                 return@flow
             }
-            resultsCache = result
+            resultsCache[competitionId] = result
             emit(Resource.Success(result))
-            emit(Resource.Loading(false))
         }
     }
 
-    fun getShooterResults(shooterId: Int): Flow<Resource<ResultsResponse, UserError>> {
+    fun getShooterResults(competitionId: Int, shooterId: Int): Flow<Resource<ResultsResponse, UserError>> {
         return flow {
             emit(Resource.Loading(true))
 
-            val cachedResults = resultsCache
+            val cachedResults = resultsCache[competitionId]
             if (cachedResults != null) {
                 val filteredResults = cachedResults.results.filter { it.signup.user.userID == shooterId.toLong() }
                 emit(Resource.Success(cachedResults.copy(results = filteredResults)))
-                emit(Resource.Loading(false))
                 return@flow
             }
 
-            emit(Resource.Loading(false))
+            val auth = "Bearer eyJ0eXA"
+            val result = try {
+                resultsRemoteDataSource.getResults(auth, competitionId)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                emit(Resource.Error(UserError.IOError))
+                return@flow
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emit(Resource.Error(UserError.HttpError))
+                return@flow
+            } catch (e: Exception) {
+                e.printStackTrace()
+                emit(Resource.Error(UserError.UnknownError))
+                return@flow
+            }
+
+            resultsCache[competitionId] = result
+
+            val filteredResults = result.results.filter { it.signup.user.userID == shooterId.toLong() }
+            emit(Resource.Success(result.copy(results = filteredResults)))
         }
     }
 }
