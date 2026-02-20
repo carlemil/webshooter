48db3d7251ada16de24a27a5e6fabbb8880615d1
Carl-Emil Kjellstrand <carl-emil.kjellstrand@thinnikatech.se>
2026-02-20 09:57:43 +0100

    Added string resources in some places.

--- app/src/main/java/se/kjellstrand/webshooter/data/common/CompetitionStatus.kt
+++ app/src/main/java/se/kjellstrand/webshooter/data/common/CompetitionStatus.kt
@@ -1,9 +1,12 @@
 package se.kjellstrand.webshooter.data.common
 
-enum class CompetitionStatus(val status: String) {
-    OPEN("open"),
-    ALL("all"),
-    COMPLETED("completed"),
-    UPCOMING("upcoming"),
-    CLOSED("closed")
+import androidx.annotation.StringRes
+import se.kjellstrand.webshooter.R
+
+enum class CompetitionStatus(val status: String, @StringRes val labelRes: Int) {
+    OPEN("open", R.string.competition_status_open),
+    ALL("all", R.string.competition_status_all),
+    COMPLETED("completed", R.string.competition_status_completed),
+    UPCOMING("upcoming", R.string.competition_status_upcoming),
+    CLOSED("closed", R.string.competition_status_closed)
 }
