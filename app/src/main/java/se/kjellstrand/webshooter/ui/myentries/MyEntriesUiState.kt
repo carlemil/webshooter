3dcbad863a5edcd7588b28a37b7731677efc2e39
Carl-Emil Kjellstrand <carl-emil.kjellstrand@thinnikatech.se>
2026-02-20 09:11:22 +0100

    Added paging for my entries.

--- app/src/main/java/se/kjellstrand/webshooter/ui/myentries/MyEntriesUiState.kt
+++ app/src/main/java/se/kjellstrand/webshooter/ui/myentries/MyEntriesUiState.kt
@@ -5,5 +5,6 @@ import se.kjellstrand.webshooter.data.competitions.remote.Datum
 data class MyEntriesUiState(
     val entries: List<Datum> = emptyList(),
     val isLoading: Boolean = true,
+    val hasMore: Boolean = true,
     val errorMessage: String? = null
 )
