4d7fc9f0c96d76393ffb434b7039b81c8855a4b7
Carl-Emil Kjellstrand <carl-emil.kjellstrand@thinnikatech.se>
2026-02-25 18:38:49 +0100

    added missing results type.

--- app/src/main/java/se/kjellstrand/webshooter/ui/results/ResultsScreen.kt
+++ app/src/main/java/se/kjellstrand/webshooter/ui/results/ResultsScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.FilterList
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
@@ -24,6 +25,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
@@ -91,6 +93,9 @@ fun CompetitionResultsScreen(
                 .padding(paddingValues)
                 .padding(horizontal = 16.dp)
         ) {
+            IconButton(onClick = { navController.popBackStack() }) {
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+            }
             Spacer(modifier = Modifier.height(16.dp))
             ResultsList(
                 resultsUiState,
